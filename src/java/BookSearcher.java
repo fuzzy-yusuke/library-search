134174c5ad6b96a2f46d7232db7e9a232070b919
fuzzy-yusuke <youth_k0228@outlook.com>
2021-08-03 20:25:54 +0900

    faces-configとweb.xmlの設定

--- src/java/BookSearcher.java
+++ src/java/BookSearcher.java
@@ -1,79 +1,65 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-import javax.inject.Named;
-import javax.enterprise.context.Dependent;
-
 import java.util.List;
 import java.util.ArrayList;
-
 import java.sql.*;
 import javax.sql.*;
-
 import javax.faces.context.FacesContext;
 import javax.faces.el.ValueBinding;
 
-@Named(value = "bookSearcher")
-@Dependent
 public class BookSearcher {
 
-    private String word="";
-    private String id="";
-    private List list=null;
-    private BookData book=null;
-    
-    public void setWord(String word){
-        this.word=word;
+    private String word = "";
+    private String id   = "";
+    private List list = null;
+    private BookData book = null;
+
+    public void setWord(String word) {
+        this.word = word;
     }
-    
-    public String getWord(){
+
+    public String getWord() {
         return word;
     }
-    
-    public List getBookList(){
+
+    public List getBookList() {
         return list;
     }
-    
-    public BookData getBookData(){
+
+    public BookData getBookData() {
         return book;
     }
-    
-    public String searchBooks(){
+
+    public String searchBooks() {
         searchBooks(word);
         return "success";
     }
-    
-    private void searchBooks(String word){
-        //データベースによる検索処理
-        list=new ArrayList();
-        try{
-            Class.forName("org.hsqldb.jdbcDriver");
-            String url="jdbc:hsqldb:hsql://localhost";
-            Connection con=DriverManager.getConnection(url,"sa","");
-            
-            String selectStatement=
-                    "select *" + 
-                    "from books where ndc           like ? "+
-                                  "or tyosya_hyouji like ?" +
-                                  "or id            like ?" +
-                                  "or title         like ?" +
-                                  "or author        like ?" +
-                                  "or publisher     like ?" ;
-            PreparedStatement prepStmt=
-                    con.prepareStatement(selectStatement);
-            prepStmt.setString(1,appendPercent(word));
-            prepStmt.setString(2,appendPercent(word));
-            prepStmt.setString(3,appendPercent(word));
-            prepStmt.setString(4,appendPercent(word));
-            prepStmt.setString(5,appendPercent(word));
-            prepStmt.setString(6,appendPercent(word));
-            
-            ResultSet rs=prepStmt.executeQuery();
-            while(rs.next()){
-                BookData book=new BookData();
+
+    private void searchBooks(String word) {
+        list = new ArrayList();
+        try {
+            Class.forName("org.mysqldb.jdbcDriver");
+            String url = "jdbc:mysqldb:hsql://localhost";
+            Connection con = DriverManager.getConnection(url, "sa", "");
+
+            String selectStatement =
+                "select * " +
+                "from books where ndc           like ? " +
+                              "or tyosya_hyouji like ? " +
+                              "or id            like ? " +
+                              "or title         like ? " +
+                              "or author        like ? " +
+                              "or publisher     like ? ";
+            PreparedStatement prepStmt =
+                con.prepareStatement(selectStatement);
+            prepStmt.setString(1, appendPercent(word));
+            prepStmt.setString(2, appendPercent(word));
+            prepStmt.setString(3, appendPercent(word));
+            prepStmt.setString(4, appendPercent(word));
+            prepStmt.setString(5, appendPercent(word));
+            prepStmt.setString(6, appendPercent(word));
+
+            ResultSet rs = prepStmt.executeQuery();
+            while (rs.next()) {
+                BookData book = new BookData();
                 book.setNdc(rs.getString("ndc"));
                 book.setTyosya_hyouji(rs.getString("tyosya_hyouji"));
                 book.setId(rs.getString("id"));
@@ -83,19 +69,20 @@ public class BookSearcher {
                 list.add(book);
             }
             prepStmt.close();
-        }catch(ClassNotFoundException e){
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
-        }catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
-    
-    private String appendPercent(String from){
-        StringBuffer to=new StringBuffer();
+
+    private String appendPercent(String from) {
+        StringBuffer to = new StringBuffer();
         to.append("%");
         to.append(from);
         to.append("%");
-        
+
         return new String(to);
     }
+
 }
--- web/search.jsp
+++ web/search.jsp
@@ -2,22 +2,24 @@
 <%@ taglib uri="http://java.sun.com/jsf/html" prefix="h" %>
 <%@ taglib uri="http://java.sun.com/jsf/core" prefix="f" %>
 
-<!DOCTYPE html>
 <html>
-    <head>
-        <link href="style.css" type="text/css" rel="stylesheet">
-        <title>}‘ŒŸõ</title>
-    </head>
-    <body>
-        <h1>}‘ŒŸõ</h1>
-        
-        <hr />
-        
-        <f:view>
-            <h:form id="serachForm">
-                <h:inputText id="searchWord" value="#{BookSeracher.word}" />
-                <h:commandButton id="submit" action="#{BookSearcher.searchBooks}" value="Go!" />
-            </h:form>
-        </f:view>
-    </body>
+<head>
+<link href="style.css" type="text/css" rel="stylesheet" />
+<title>}‘ŒŸõ</title>
+</head>
+<body>
+
+<h1>}‘ŒŸõ</h1>
+
+<hr />
+
+<f:view>
+    <h:form id="searchForm">
+        <h:inputText id="searchWord" value="#{BookSearcher.word}" />
+        <h:commandButton id="submit"
+             action="#{BookSearcher.searchBooks}" value="Go!" />
+    </h:form>
+</f:view>
+
+</body>
 </html>
